449468eebb0fc57c89acf1fc3df66501d035915f
sqfasd <sqf1225@foxmail.com>
2014-12-05 14:40:31 +0800

    adjust directory structure

---
+++ tcpmany.h
@@ -0,0 +1,9 @@
+#ifndef TCPMANY_TCPMANY_H_
+#define TCPMANY_TCPMANY_H_
+
+#include "kernel.h"
+#include "connection.h"
+#include "packet.h"
+#include "inet_address.h"
+
+#endif  // TCPMANY_TCPMANY_H_
